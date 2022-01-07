371634687e501d0d1290c3fea0b08b5586df0656
Jesus Gollonet <jesusgollonet@gmail.com>
2022-01-07 09:56:15 +0000

    Initial terraform config

---
+++ instances/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.26.0"
+    }
+  }
+  required_version = ">= 0.14.5"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "rde" {
+  ami           = var.ami
+  instance_type = "t3.micro"
+}
---
+++ instances/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "Region for the instance"
+}
+
+variable "ami" {
+  description = "AMI for the instance"
+}
